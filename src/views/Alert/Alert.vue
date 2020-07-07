e261f939692a2f74768228985a30f9a192a2d46a
wxc1601030089 <1498368314@qq.com>
2020-07-07 14:40:58 +0800

    update

---
+++ src/views/Alert/Alert.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class = "Alert">
+    告警
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Alert",
+      data(){
+        return {
+
+        }
+      },
+      methods:{
+
+      }
+    }
+</script>
+
+<style scoped>
+  .write {
+    outline: none;
+    width: 30px;
+    border-top-width: 0px;
+    border-left-width: 0px;
+    border-right-width: 0px;
+    border-bottom-width: 1px;
+  }
+</style>
